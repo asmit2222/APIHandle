f4712c5289d7f685adcf1d9d66a6547b59057b45
Asmit <asmitkatiyar33@gmail.com>
2024-12-26 21:56:39 +0530

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,25 +75,19 @@ function Navbar() {
 
         <div className="sm:hidden" id="mobile-menu">
           <div className="space-y-1 px-2 pb-3 pt-2">
-            <a
-              href="#"
+            <Link
+              to="/APIHandle"
               className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
               aria-current="page"
             >
               Team Page
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/APIHandle/testemonial"
               className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
             >
               Testemonials
-            </a>
-            <a
-              href="#"
-              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-            >
-              Product details
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
